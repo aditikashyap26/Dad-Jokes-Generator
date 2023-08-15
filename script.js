a220811b6a344a8acfd0cecd63d809c4533012cc
Aditi <aditikashyap387@gmail.com>
2023-08-15 22:15:13 +0530

    added script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const btnE1 = document.getElementById("btn")
+const btnEl = document.getElementById("btn")
+const jokeEl = document.getElementById("joke")
 
 const apikey = "Aev3NcSZuGG6g7xmSA6hrw==qQGin5jKFPw7Pmcq";
 
@@ -11,11 +12,29 @@ const options = {
 
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
-async function getJoke(){
-    const response = fetch(apiURL, options)
-    const data = await response.json()
+async function getJoke() {
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+    
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+    
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+        
+        jokeEl.innerText = data[0].joke;
+    } catch(error) {
+        jokeEl.innerText = "An error happened, try again later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+    }
 
-    console.log(data);
+    
+    
+
+    
 }
 
-btnE1.addEventListener("click", getJoke)
+btnEl.addEventListener("click", getJoke)
